398c590501d8cdcd055065ec5b34c9140ac12c8c
Reedwan7 <4balor86@solent.ac.uk>
2021-11-02 20:58:55 +0000

    added code display message

---
+++ basics/ascii_art.py
@@ -0,0 +1,11 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+print("\n")
+print("##########")
+print("#  *  *  #")
+print("#    w   #")
+print("#   __   #")
+print("##########")
