691ef7abad88b29d5ef2c0f6d1bfc35734b00e42
Reedwan7 <4balor86@solent.ac.uk>
2021-11-15 21:34:44 +0000

    added code display message

---
+++ basics/decisions/simple_decisions/if_else.py
@@ -0,0 +1,9 @@
+print("enter an activity")
+activity = input()
+# Determine if the activity is "calculate"
+if activity == "calculate":
+    print("performing calculations...")
+print("\n")
+print("Activity completed!")
+
+
