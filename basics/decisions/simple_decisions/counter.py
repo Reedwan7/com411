55d36277682e0e15799397c775296c0ad3358e64
Reedwan7 <4balor86@solent.ac.uk>
2021-11-16 22:08:56 +0000

    added code display message

--- basics/decisions/simple_decisions/counter.py
+++ basics/decisions/simple_decisions/counter.py
@@ -8,9 +8,26 @@ print("please enter third number")
 number_3 = int(input())
 print("\n")
 # Determine the odd numbers
-even_number = 0
-odd_number = 0
+even_numbers = 0
+odd_numbers = 0
 print("\n")
-if number_1
+if number_1 % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+print("\n")
+if number_2 % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+print("\n")
+if number_3 % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+print("\n")
+# Display result
+print(f"there were {odd_numbers} odd number and {even_numbers} even number.")
+
 
 
