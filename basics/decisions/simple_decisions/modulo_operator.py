47f9a8c64aaa45a53fefef99094e25eec83e8379
Reedwan7 <4balor86@solent.ac.uk>
2021-11-16 00:40:10 +0000

    added code display message

---
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -0,0 +1,11 @@
+print("please enter a whole number.")
+number = int(input())
+print("\n")
+# Determine if number is odd
+if number % 2 == 0:
+    print(f"The number {number} is not an odd number")
+else:
+    print(f"The number {number} is an odd number")
+
+
+
