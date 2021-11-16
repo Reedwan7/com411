7e55a3a27c5021e3df411c3bda9f4534c637701e
Reedwan7 <4balor86@solent.ac.uk>
2021-11-16 01:28:21 +0000

    added code display message

---
+++ basics/decisions/simple_decisions/counter.py
@@ -0,0 +1,16 @@
+print("please enter first number")
+number_1 = int(input())
+print("\n")
+print("please enter second number")
+number_2 = int(input())
+print("\n")
+print("please enter third number")
+number_3 = int(input())
+print("\n")
+# Determine the odd numbers
+even_number = 0
+odd_number = 0
+print("\n")
+if number_1
+
+
