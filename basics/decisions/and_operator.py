cc4ae788ff058fcd879105eab8158100fd4d2bea
4balor86 <4balor86@solent.ac.uk>
2021-11-25 16:25:58 +0000

    added code display message

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+# Retrieve hearing & seeing from  user
+print("What did i hear?")
+hear = input()
+print("What did i see?")
+see = input()
+# Determine response
+if hear == ('grrr') and see == ('two red eyes'):
+    print("There is a scary creature. I should get out of here")
+else:
+    print("I am a little scared but I will continue")
+
